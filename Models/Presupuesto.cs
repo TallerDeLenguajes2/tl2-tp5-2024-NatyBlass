1e59a49f976ae31908d45707a2882f66372f53a6
NatyBlass <natypaz12345@gmail.com>
2024-11-08 11:16:31 -0300

    Agrego la Lista detalle a la clase Presupuesto

--- Models/Presupuesto.cs
+++ Models/Presupuesto.cs
@@ -3,16 +3,19 @@ public class Presupuesto
     private int idPresupuesto;
     private string nombreDestinatario;
     private DateTime fechaCreacion;
+    private List<PresupuestoDetalle> detalle;
 
     public int IdPresupuesto { get => idPresupuesto; set => idPresupuesto = value; }
     public string NombreDestinatario { get => nombreDestinatario; set => nombreDestinatario = value; }
     public DateTime FechaCreacion { get => fechaCreacion; set => fechaCreacion = value; }
+    public List<PresupuestoDetalle> Detalle { get => detalle; set => detalle = value; }
 
     public Presupuesto(int idPresupuesto, string nombreDestinatario, DateTime fechaCreacion)
     {
         this.idPresupuesto = idPresupuesto;
         this.nombreDestinatario = nombreDestinatario;
         this.fechaCreacion = fechaCreacion;
+        this.detalle = new List<PresupuestoDetalle>();
     }
-    
+
 }
--- Models/PresupuestoDetalle.cs
+++ Models/PresupuestoDetalle.cs
@@ -3,7 +3,7 @@ public class PresupuestoDetalle
     private Producto prod;
     private int cantidad;
 
-    public Producto Prod { get => producto; set => producto = value; }
+    public Producto Prod { get => prod; set => prod = value; }
     public int Cantidad { get => cantidad; set => cantidad = value; }
 
     public PresupuestoDetalle(Producto prod, int cantidad)
